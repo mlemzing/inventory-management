dc5da5cd902f93b6ccf8ba37ccc1927e70f83dc5
Ying <ying1996@gmail.com>
2024-06-19 00:38:27 +0800

    initial commit

--- item_cdk/item_cdk_stack.py
+++ item_cdk/item_cdk_stack.py
@@ -1,19 +1,22 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_dynamodb as dynamodb,
+    aws_apigatewayv2 as apigateway,
+    aws_lambda as _lambda,
 )
 from constructs import Construct
+from item_cdk.database_stack import DatabaseStack
+from item_cdk.http_api_stack import HttpApiStack
+from item_cdk.routes_stack import RoutesStack
+
 
 class ItemCdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ItemCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self._database_stack = DatabaseStack(self, "DatabaseStack")
+        self._http_api_stack = HttpApiStack(self, "HttpApiStack")
+        self._routes_stack = RoutesStack(
+            self, "RoutesStack", self._http_api_stack, self._database_stack)
